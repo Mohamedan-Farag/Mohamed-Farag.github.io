97db23e38356d4173f99d0981b2bb1fd8938c7da
Mohamedan Farag <120517880+Mohamedan-Farag@users.noreply.github.com>
2024-02-13 00:06:27 +0200

    Add files via upload

---
+++ scripts/projects-handler.js
@@ -0,0 +1,42 @@
+var projectsDiv = document.getElementById("projects-div");
+for (var i = 0; i < PROJECTS.length; i++) {
+  // div
+  var projectDiv = document.createElement("div");
+  var divStyle = document.createAttribute("style");
+  divStyle.value = "display:inline-block; width: 300px; margin: 10px;";
+  projectDiv.setAttributeNode(divStyle);
+
+  // h3
+  var title = document.createElement("h3");
+  var titleNode = document.createTextNode(PROJECTS[i].title);
+  title.appendChild(titleNode);
+
+  // img
+  var image = document.createElement("img");
+  var source = document.createAttribute("src");
+  source.value = "./images/" + PROJECTS[i].img;
+  image.setAttributeNode(source);
+  var style = document.createAttribute("style");
+  style.value = "width: 180px; height: 100px;";
+  image.setAttributeNode(style);
+
+  // p
+  var p = document.createElement("p");
+  var node = document.createTextNode(PROJECTS[i].description);
+  p.appendChild(node);
+  
+    // create an anchor tag for span
+  var a = document.createElement("a");
+  var lk = document.createTextNode(PROJECTS[i].link);
+  a.href = PROJECTS[i].link;
+  a.innerHTML = "Project Link";
+
+
+
+  projectDiv.appendChild(title);
+  projectDiv.appendChild(image);
+  projectDiv.appendChild(p); // append the span element
+  projectDiv.appendChild(a);
+
+  projectsDiv.appendChild(projectDiv);
+}
---
+++ scripts/projects.js
@@ -0,0 +1,22 @@
+var PROJECTS = [
+  {
+    img: "ct_4.png",
+    title: "Connect_4",
+    description: "",
+    link: "https://github.com/Mohamedan-Farag/Connect_4_AI",
+  },
+
+  {
+    img: "x_o.png",
+    title: "TIC_TAC_TOE",
+    description: "",
+    link: "https://github.com/Mohamedan-Farag/TIC_TAC_TOE",
+  },
+  {
+    img: "c_m.png",
+    title: "Contact management system project",
+    description: "",
+    link: "https://github.com/Mohamedan-Farag/Contact",
+  },
+
+];
